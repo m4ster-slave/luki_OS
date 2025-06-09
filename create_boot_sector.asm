1225fd7d783c2ca7f6c8a4cbf8991f3f9f7e3bc4
m4ster-slave <mail@weger.dev>
2025-06-10 01:39:10 +0200

    print hello world

--- create_boot_sector.asm
+++ create_boot_sector.asm
@@ -1,8 +1,31 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop 
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
 
-; Fill with 510 zeros minus the size of the previous code
-times 510-($-$$) db 0
-; Magic number
+
+
+
+
+jmp $ ; jump to current address = infinite loop
+
+times 510 - ($-$$) db 0
 dw 0xaa55 
